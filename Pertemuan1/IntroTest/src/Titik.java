307cc2a12532a5f529117454ab72ad36b7cee15c
Izzatu Khoirul Fata <izatukhoirulfata14@gmail.com>
2026-02-20 11:20:41 +0700

    MTitik

---
+++ Pertemuan1/IntroTest/src/Titik.java
@@ -0,0 +1,39 @@
+public class Titik {
+    // Atribut
+    double absis;
+    double ordinat;
+
+    // METHOD
+    // Konstruktor membuat titik (0,0)
+    Titik (){
+        absis = 0;
+        ordinat = 0;
+    }
+
+    //Mengembalikan nilai absis
+    double getAbsis(){
+        return absis;
+    }
+    //Mengembalikan nilai Ordinat
+    double getOrdinat(){
+        return Ordinat;
+    }
+
+    //Mengeset absis dengan nilai baru x
+    void setAbsis(double x){
+        absis = x;
+    }
+    //Mengeset Ordinat
+    void setOrdinat(double y){
+        ordinat = y;
+    }
+
+    void geser(double x, double y){
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+
+    void printTitik() {
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
+    }
+}//end class titik

02b8c70c244c8544f0b76b2c37380032007ef7c0
Izzatu Khoirul Fata <izatukhoirulfata14@gmail.com>
2026-02-20 13:29:21 +0700

    fix class titik

--- Pertemuan1/IntroTest/src/Mtitik.java
+++ Pertemuan1/IntroTest/src/Mtitik.java
@@ -1,10 +1,15 @@
+/* Nama File        : MTitik.java
+ * Deskripsi        : berisi Main untuk class titik
+ * Pembuat          : Izzatu Khoirul Fata/24060124120039
+ * Tanggal          : 20 Februari 2026 */
+
 public class MTitik {
     public static void main(String [] args) {
-        Titik T1 = new Titik();
-        T1.setAbsis(3);
-        T1.setOrdinat(4);
-        T1.printTitik();
-        T1.geser(3,4);
-        T1.printTitik();
+        Titik T1 = new Titik(); //Membuat objek titik T1(0,0)
+        T1.setAbsis(3); //Mengubah absis T1 dengan nilai 3
+        T1.setOrdinat(4);  //Mengubah ordinat T1 dengan nilai 4
+        T1.printTitik(); //Mencetak koordinat titik T1
+        T1.geser(3,4); // Menggeser T1 sejauh (3,4)
+        T1.printTitik(); //Menampillkan koordinat T1 seletah digeser 0
     }
 }
--- Pertemuan1/IntroTest/src/Titik.java
+++ Pertemuan1/IntroTest/src/Titik.java
@@ -1,3 +1,8 @@
+/* Nama File       : Titik.java
+ * Deskripsi       : berisi atribut dan metode dalam class titik
+ * Pembuat          : Izzatu Khoirul Fata/24060124120039
+ * Tanggal          : 20 Februari 2026 */
+
 public class Titik {
     // Atribut
     double absis;
@@ -5,7 +10,7 @@ public class Titik {
 
     // METHOD
     // Konstruktor membuat titik (0,0)
-    Titik (){
+    Titik(){
         absis = 0;
         ordinat = 0;
     }
@@ -19,20 +24,22 @@ public class Titik {
         return Ordinat;
     }
 
-    //Mengeset absis dengan nilai baru x
+    //Mengeset absis titik dengan nilai baru x
     void setAbsis(double x){
         absis = x;
     }
-    //Mengeset Ordinat
+    //Mengeset Ordinat titik dengan nilai baru y
     void setOrdinat(double y){
         ordinat = y;
     }
 
+    //MEngerser nilai absis dan ordinat masing masing sejauh x dan y
     void geser(double x, double y){
         absis = absis + x;
         ordinat = ordinat + y;
     }
 
+    //Mencetak koordinat titk
     void printTitik() {
         System.out.println("Titik (" + absis + "," + ordinat + ")");
     }
